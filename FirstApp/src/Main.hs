ccbf6f07913b1ebe49aadf3e577b4fdf3d68d74d
vt4a2h <vt4a2h@gmail.com>
2014-07-02 23:34:56 +0700

    higher-order functions example

--- FirstApp/src/Main.hs
+++ FirstApp/src/Main.hs
@@ -1,22 +1,65 @@
-simple_sum :: Int -> Int
-simple_sum value = value + value
+--simple_sum :: Int -> Int
+--simple_sum value = value + value
 
-indicate :: String -> String
-indicate address
-    | address == "127.0.0.1" = "localhost"
-    | null address = "empty IP-address"
-    | otherwise = address
+--indicate :: String -> String
+--indicate address
+--    | address == "127.0.0.1" = "localhost"
+--    | null address = "empty IP-address"
+--    | otherwise = address
 
-prepare_length :: Double -> Double
-prepare_length line = 
-    line * coefficient
-    where coefficient = 0.4959
+--prepare_length :: Double -> Double
+--prepare_length line = 
+--    line * coefficient
+--    where coefficient = 0.4959
 
-prepare_length_let :: Double -> Double
-prepare_length_let line = 
-    let coefficient = 12.4959
-        correction = 0.0012
-    in
-    line * coefficient - correction
+--prepare_length_let :: Double -> Double
+--prepare_length_let line = 
+--    let coefficient = 12.4959
+--        correction = 0.0012
+--    in
+--    line * coefficient - correction
 
-main = putStrLn (show ((\x -> x*x) 5))
+--main = 
+--	print (f 5)
+--	where f = \x -> x*x
+
+type Login = String
+type Password = String
+type AvatarURL = String
+type UserId = Integer
+
+user_info :: Login -> Password -> AvatarURL -> UserId -> String
+user_info login password avatar_URL user_id =
+	"Fullinfo about user @" ++ (show user_id) ++ ":" ++
+	"\n login: " ++ login ++
+	"\n password: " ++ password ++
+	"\n avatar url: " ++ avatar_URL
+
+type EmptyInfo = Login -> Password -> AvatarURL -> UserId -> String
+type WithLogin = Password -> AvatarURL -> UserId -> String
+type AndWithPassword = AvatarURL ->UserId -> String
+type AndWithAvatarURL = UserId -> String
+
+store_login_in :: EmptyInfo -> UserId -> WithLogin
+store_login_in empty_info user_id = 
+	empty_info "vitaly"
+	-- get login operation depends on user_id
+
+store_password_in :: WithLogin -> UserId -> AndWithPassword
+store_password_in info_with_login user_id = 
+	info_with_login "123456789abc"
+	-- get password operation depends on user_id
+
+store_avatar_URL_in :: AndWithPassword -> UserId -> AndWithAvatarURL
+store_avatar_URL_in info_with_password user_id =
+	info_with_password "http://foo.bar.png/"
+	-- get url operation depends on user_id
+
+main = 
+	let user_id = 1234
+	    info_with_login = store_login_in user_info user_id
+	    info_with_password = store_password_in info_with_login user_id
+	    info_with_avatar_URL = store_avatar_URL_in info_with_password user_id
+	    full_info_about_user = info_with_avatar_URL user_id
+	in
+	putStrLn full_info_about_user
