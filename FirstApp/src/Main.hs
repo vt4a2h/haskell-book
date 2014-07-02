baf224f1055ebbd012ebf0bd2886ac065512265d
vt4a2h <vt4a2h@gmail.com>
2014-07-02 23:00:50 +0700

    simple lambda

--- FirstApp/src/Main.hs
+++ FirstApp/src/Main.hs
@@ -1,3 +1,22 @@
-import Helpers
+simple_sum :: Int -> Int
+simple_sum value = value + value
 
-main = putStrLn (hello "vitaly")
+indicate :: String -> String
+indicate address
+    | address == "127.0.0.1" = "localhost"
+    | null address = "empty IP-address"
+    | otherwise = address
+
+prepare_length :: Double -> Double
+prepare_length line = 
+    line * coefficient
+    where coefficient = 0.4959
+
+prepare_length_let :: Double -> Double
+prepare_length_let line = 
+    let coefficient = 12.4959
+        correction = 0.0012
+    in
+    line * coefficient - correction
+
+main = putStrLn (show ((\x -> x*x) 5))
